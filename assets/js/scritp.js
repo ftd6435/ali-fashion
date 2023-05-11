898b4ee070a40231c8d56b2ea6b7113542fb0342
ftd6435 <mamadoupathediallo71@gmail.com>
2023-05-11 03:59:08 +0000

    Single pages added Services about contact gallery

--- assets/js/scritp.js
+++ assets/js/scritp.js
@@ -28,10 +28,84 @@ var instragramSlider = new Swiper('.instagram-slider', {
 
 
 let navbar = document.querySelector('.navbar');
-let navOnglets = document.querySelectorAll('.nav-link');
 let navbarMenu = document.querySelector('.navbar-nav');
 let closeMenuv = document.querySelector('.close-bars');
 let openMenuv = document.querySelector('.menu-bars');
+
+const galleryItem = document.querySelectorAll('.gallery-item');
+// for container
+const boxContenainer = document.createElement('div');
+// for basic area 
+const boxContent = document.createElement('div');
+// for images
+const boxImg = document.createElement('img');
+// for prev button to change images 
+const btnPrev = document.createElement('div');
+// for next button to change images 
+const btnNext = document.createElement('div');
+
+
+// create classList 
+boxContenainer.classList.add('lightbox');
+boxContent.classList.add('lightbox-content');
+btnPrev.classList.add('fa', 'fa-angle-left', 'lightbox-prev');
+btnNext.classList.add('fa', 'fa-angle-right', 'lightbox-next');
+
+boxContenainer.appendChild(boxContent);
+boxContent.appendChild(boxImg);
+boxContent.appendChild(btnPrev);
+boxContent.appendChild(btnNext);
+
+document.body.appendChild(boxContenainer);
+
+let index = 1;
+
+function showLightBox(n){
+    if(n > galleryItem.length){
+        index = 1
+    }else if(n < 1){
+        index = galleryItem.length;
+    }
+
+    let imageLocation = galleryItem[index-1].getAttribute("src");
+    boxImg.setAttribute('src', imageLocation);
+}
+
+
+function currentImage(){
+    boxContenainer.style.display = "block";
+    let imageIndex = parseInt(this.getAttribute('data-index'));
+    showLightBox(index = imageIndex);
+}
+
+for(let i = 0; i < galleryItem.length; i++){
+    galleryItem[i].addEventListener('click', currentImage);
+}
+
+function sliderImage(n){
+    showLightBox(index +=n);
+}
+
+function prevImage(){
+    sliderImage(-1);
+}
+
+function nextImage(){
+    sliderImage(1);
+}
+
+btnPrev.addEventListener('click', prevImage);
+btnNext.addEventListener('click', nextImage);
+
+function closeBox(){
+    if(this === event.target){
+        boxContenainer.style.display = 'none';
+    }
+}
+
+boxContenainer.addEventListener('click', closeBox);
+
+
 
 function openMenu(){
     navbarMenu.style.clipPath = "polygon(0 0, 100% 0, 100% 100%, 0 100%)";
@@ -45,24 +119,12 @@ function closeMenu(){
     openMenuv.style.display = "inline-block";
 }
 
-// scroll mooth after click
-navOnglets.forEach(nav => {
-    nav.addEventListener('click', function(e){
-        let id = e.target.getAttribute('data-sec');
-
-        navOnglets.forEach(n => {
-            let test = n.getAttribute('data-sec');
-            test == id ? n.classList.add('active') : n.classList.remove('active');;
-
-            document.querySelector(`.section-${id}`).scrollIntoView({behavior: "smooth"});
-        });
-    });
-});
-
 // sticky navbar 
 window.addEventListener('scroll', function stickyFunction (){
+    console.log('start');
     if(window.scrollY >= 100){
         navbar.classList.add('sticky');
+        console.log('in');
     }else{
         navbar.classList.remove('sticky');
     }
